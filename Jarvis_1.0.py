cb8c36ca29b43af0edd6a7ec5c937c59c93223ca
Sebastian Cuello <axel-cuello@hotmail.com>
2021-03-19 01:09:28 -0300

    Implementacion de funciones del tiempo

--- Jarvis_1.0.py
+++ Jarvis_1.0.py
@@ -1,9 +1,30 @@
 # Para linux instalar 
 # pyttsx3 y libespeak1 (sudo)
 
-
 import pyttsx3
+import datetime
 
 engine = pyttsx3.init()
-engine.say('Hello world!')
-engine.runAndWait()
+voice = 'english'
+
+# Función que recibe un string y reproduce en audio dicho string
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+# Función que nos indica el tiempo actual (Tomado desde la computadora)
+def time_():
+    Time = datetime.datetime.now().strftime("%H:%M:%S") # Reloj 24 horas
+    speak("The current time is")
+    speak(Time)
+
+# Función que nos indica la fecha actual (Tomado desde la computadora)
+def date_():
+    year = datetime.datetime.now().year
+    month = datetime.datetime.now().month
+    date = datetime.datetime.now().day
+    speak("The current date is")
+    speak(date)
+    speak(month)
+    speak(year)
+
